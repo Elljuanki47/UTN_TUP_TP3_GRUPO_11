13feddfdd23d7e7c34cff78d05ec91897f35b072
Juan Ignacio Zarate <jizarate47@outlook.com>
2026-09-07 14:45:48 -0300

    Agrego validacion inidical de localidades

---
+++ TP3_GRUPO_11/Ejercicio1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="TP3_GRUPO_11.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <h2>Localidades</h2>
+
+        <p>
+            <asp:Label ID="lblNombreLocalidad" runat="server"
+                Text="Nombre de localidad:"
+                AssociatedControlID="txtLocalidad" />
+
+            <asp:TextBox ID="txtLocalidad" runat="server"
+                Width="145px" />
+
+            <asp:RequiredFieldValidator ID="rfvLocalidad" runat="server"
+                ControlToValidate="txtLocalidad"
+                Text="*"
+                ErrorMessage="Ingresar localidad"
+                ValidationGroup="Localidad">
+            </asp:RequiredFieldValidator>
+        </p>
+
+        <p>
+            <asp:Button ID="btnGuardarLocalidad" runat="server"
+                Text="Guardar localidad"
+                Width="145px"
+                ValidationGroup="Localidad" />
+        </p>
+
+        <asp:ValidationSummary ID="vsLocalidad" runat="server"
+            HeaderText="Revise los siguientes errores:"
+            ShowSummary="True"
+            ShowMessageBox="True"
+            ValidationGroup="Localidad" />
+
+    </form>
+</body>
+</html>
---
+++ TP3_GRUPO_11/Ejercicio1.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP3_GRUPO_11
+{
+
+
+    public partial class Ejercicio1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblNombreLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombreLocalidad;
+
+        /// <summary>
+        /// Control txtLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLocalidad;
+
+        /// <summary>
+        /// Control rfvLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvLocalidad;
+
+        /// <summary>
+        /// Control btnGuardarLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardarLocalidad;
+
+        /// <summary>
+        /// Control vsLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary vsLocalidad;
+    }
+}

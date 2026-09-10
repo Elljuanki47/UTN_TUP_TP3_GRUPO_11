9f690da6d01b12f497029aa279720b9405daf572
Delesito <deledicquejoaquin@gmail.com>
2026-09-10 08:01:03 -0300

    tp3 mod v3

---
+++ TP3_GRUPO_11/Ejercicio1.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="TP3_GRUPO_11.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <h2>Localidades</h2>
+
+        <p>
+            <asp:Label ID="lblNombreLocalidad" runat="server"
+                Text="Nombre de localidad:"
+                AssociatedControlID="txtLocalidad" />
+
+            <asp:TextBox ID="txtLocalidad" runat="server"
+                Width="145px" />
+
+            <asp:RequiredFieldValidator ID="rfvLocalidad" runat="server"
+                ControlToValidate="txtLocalidad"
+                Text="*"
+                ErrorMessage="Ingresar localidad"
+                ValidationGroup="Localidad">
+            </asp:RequiredFieldValidator>
+        </p>
+
+        <p>
+            <asp:Button ID="btnGuardarLocalidad" runat="server"
+                Text="Guardar localidad"
+                Width="145px"
+                ValidationGroup="Localidad" />
+        </p>
+
+        <asp:ValidationSummary ID="vsLocalidad" runat="server"
+            HeaderText="Revise los siguientes errores:"
+            ShowSummary="True"
+            ShowMessageBox="True"
+            ValidationGroup="Localidad" />
+
+        <h2>Usuarios</h2>
+
+        <p>Nombre usuario: <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox></p>
+
+        <p>Contraseña: <asp:TextBox ID="txtContrasena" runat="server" TextMode="Password"></asp:TextBox></p>
+
+        <p>Repetir contraseña: <asp:TextBox ID="txtRepetirContrasena" runat="server" TextMode="Password"></asp:TextBox></p>
+
+        <p>Correo electrónico: <asp:TextBox ID="txtCorreo" runat="server"></asp:TextBox></p>
+ 
+        <p>CP: <asp:TextBox ID="txtCP" runat="server"></asp:TextBox></p>
+        <p>Localidades:
+            <asp:DropDownList ID="ddlLocalidades" runat="server">
+            </asp:DropDownList>
+        </p>
+
+        <asp:Button ID="btnGuardarUsuario" runat="server" style="margin-left: 87px" Text="Guardar Usuario" Width="120px" />
+        <p>
+            <asp:Button ID="btnInicio" runat="server" Text="Ir a inicio.aspx" />
+        </p>
+
+    </form>
+</body>
+</html>
---
+++ TP3_GRUPO_11/Ejercicio1.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP3_GRUPO_11
+{
+
+
+    public partial class Ejercicio1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblNombreLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombreLocalidad;
+
+        /// <summary>
+        /// Control txtLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLocalidad;
+
+        /// <summary>
+        /// Control rfvLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvLocalidad;
+
+        /// <summary>
+        /// Control btnGuardarLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardarLocalidad;
+
+        /// <summary>
+        /// Control vsLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary vsLocalidad;
+
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+
+        /// <summary>
+        /// Control txtContrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContrasena;
+
+        /// <summary>
+        /// Control txtRepetirContrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRepetirContrasena;
+
+        /// <summary>
+        /// Control txtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+
+        /// <summary>
+        /// Control txtCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCP;
+
+        /// <summary>
+        /// Control ddlLocalidades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlLocalidades;
+
+        /// <summary>
+        /// Control btnGuardarUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardarUsuario;
+
+        /// <summary>
+        /// Control btnInicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnInicio;
+    }
+}
